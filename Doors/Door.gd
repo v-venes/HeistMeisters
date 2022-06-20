29d9cb85de6145aa06dc861041d1f818e10e9e1b
Victor Neves <victor.neves4@fatec.sp.gov.br>
2022-06-19 23:49:59 -0300

    :sparkles: add doors

---
+++ Doors/Door.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var can_open = false
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_Area2D_body_entered(body):
+	can_open = true
+	open_door()
+
+func _on_Area2D_body_exited(body):
+	can_open = false
+
+func open_door():
+	$AnimationPlayer.play("Open")
